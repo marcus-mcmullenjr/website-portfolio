27e0d096f5da07889bee73cd3d83c0c18819b3b7
marcus-mcmullenjr <marcus.mcmullenjr@gmail.com>
2025-06-10 17:50:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+var navLinks = document.getElementById("navLinks");
+
+function showMenu(){
+	navLinks.style.right = "0";
+}
+			
+function hideMenu(){
+	navLinks.style.right = "-200px";
+}
+            
 function toggleMenu(){
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
